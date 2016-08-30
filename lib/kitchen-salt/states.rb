8ceda199a828fc762f816bb11edc03382f56baaa
Joshua B. Bussdieker <jbussdieker@gmail.com>
2016-08-29 22:04:51 -0700

    Refactor

---
+++ lib/kitchen-salt/states.rb
@@ -0,0 +1,100 @@
+module Kitchen
+  module Salt
+    module States
+      private
+
+      def prepare_state_top
+        info('Preparing state_top')
+
+        sandbox_state_top_path = File.join(sandbox_path, config[:salt_state_top])
+
+        if config[:state_top_from_file] == false
+          # use the top.sls embedded in .kitchen.yml
+
+          # we get a hash with all the keys converted to symbols, salt doesn't like this
+          # to convert all the keys back to strings again
+          state_top_content = unsymbolize(config[:state_top]).to_yaml
+          # .to_yaml will produce ! '*' for a key, Salt doesn't like this either
+          state_top_content.gsub!(/(!\s'\*')/, "'*'")
+        else
+          # load a top.sls from disk
+          state_top_content = File.read('top.sls')
+        end
+
+        write_raw_file(sandbox_state_top_path, state_top_content)
+      end
+
+      def prepare_states
+        if config[:state_collection] || config[:is_file_root]
+          prepare_state_collection
+        else
+          prepare_formula config[:kitchen_root], config[:formula]
+          prepare_vendor_states
+        end
+
+        config[:dependencies].each do |formula|
+          prepare_formula formula[:path], formula[:name]
+        end
+      end
+
+      def prepare_vendor_states
+        vendor_path = config[:vendor_path]
+
+        unless vendor_path.nil?
+          if Pathname.new(vendor_path).exist?
+            Dir[File.join(vendor_path, '*')].each do |d|
+              prepare_formula vendor_path, File.basename(d)
+            end
+          else
+            # :vendor_path was set, but not valid
+            raise UserError, "kitchen-salt: Invalid vendor_path set: #{vendor_path}"
+          end
+        end
+      end
+
+      def prepare_formula_dir(path, subdir)
+        src = File.join(path, subdir)
+
+        if File.directory?(src)
+          debug("prepare_formula_dir: #{src} exists, copying..")
+          subdir_path = File.join(sandbox_path, config[:salt_file_root], subdir)
+          FileUtils.mkdir_p(subdir_path)
+          cp_r_with_filter(src, subdir_path, config[:salt_copy_filter])
+        else
+          debug("prepare_formula_dir: #{src} doesn't exist, skipping.")
+        end
+      end
+
+      def prepare_formula(path, formula)
+        info("Preparing formula: #{formula} from #{path}")
+        debug("Using config #{config}")
+
+        formula_dir = File.join(sandbox_path, config[:salt_file_root], formula)
+        FileUtils.mkdir_p(formula_dir)
+        cp_r_with_filter(File.join(path, formula), formula_dir, config[:salt_copy_filter])
+
+        # copy across the _modules etc directories for python implementation
+        %w(_modules _states _grains _renderers _returners).each do |extrapath|
+          prepare_formula_dir(path, extrapath)
+        end
+      end
+
+      def prepare_state_collection
+        info('Preparing state collection')
+        collection_name = config[:collection_name]
+        formula = config[:formula]
+
+        if collection_name.nil? && formula.nil?
+          info('neither collection_name or formula have been set, assuming this is a pre-built collection')
+          collection_name = ''
+        elsif collection_name.nil?
+          collection_name = formula
+        end
+
+        collection_dir = File.join(sandbox_path, config[:salt_file_root], collection_name)
+        FileUtils.mkdir_p(collection_dir)
+        cp_r_with_filter(config[:kitchen_root], collection_dir, config[:salt_copy_filter])
+      end
+    end
+  end
+end
--- lib/kitchen/provisioner/salt_solo.rb
+++ lib/kitchen/provisioner/salt_solo.rb
@@ -18,6 +18,8 @@
 
 require 'kitchen/provisioner/base'
 require 'kitchen-salt/util'
+require 'kitchen-salt/pillars'
+require 'kitchen-salt/states'
 require 'fileutils'
 require 'yaml'
 
@@ -29,54 +31,55 @@ module Kitchen
 
     class SaltSolo < Base
       include Kitchen::Salt::Util
+      include Kitchen::Salt::Pillars
+      include Kitchen::Salt::States
 
-      default_config :salt_version, 'latest'
-
-      # supported install methods: bootstrap|apt
-      default_config :salt_install, 'bootstrap'
-
-      default_config :salt_bootstrap_url, 'http://bootstrap.saltstack.org'
-      default_config :salt_bootstrap_options, ''
-
-      # alternative method of installing salt
-      default_config :salt_apt_repo, 'http://apt.mccartney.ie'
-      default_config :salt_apt_repo_key, 'http://apt.mccartney.ie/KEY'
-      default_config :salt_ppa, 'ppa:saltstack/salt'
-
-      default_config :chef_bootstrap_url, 'https://www.getchef.com/chef/install.sh'
-
-      default_config :salt_config, '/etc/salt'
-      default_config :salt_minion_config, '/etc/salt/minion'
-      default_config :salt_env, 'base'
-      default_config :salt_file_root, '/srv/salt'
-      default_config :salt_pillar_root, '/srv/pillar'
-      default_config :salt_state_top, '/srv/salt/top.sls'
-      default_config :state_collection, false
-      default_config :state_top, {}
-      default_config :state_top_from_file, false
-      default_config :salt_run_highstate, true
-      default_config :salt_copy_filter, []
-      default_config :is_file_root, false
-      default_config :require_chef, true
-
-      default_config :dependencies, []
-      default_config :vendor_path, nil
-      default_config :omnibus_cachier, false
+      DEFAULT_CONFIG = {
+        salt_version: 'latest',
+        salt_install: 'bootstrap',
+        salt_bootstrap_url: 'http://bootstrap.saltstack.org',
+        salt_bootstrap_options: '',
+        salt_apt_repo: 'http://apt.mccartney.ie',
+        salt_apt_repo_key: 'http://apt.mccartney.ie/KEY',
+        salt_ppa: 'ppa:saltstack/salt',
+        chef_bootstrap_url: 'https://www.getchef.com/chef/install.sh',
+        salt_config: '/etc/salt',
+        salt_minion_config: '/etc/salt/minion',
+        salt_env: 'base',
+        salt_file_root: '/srv/salt',
+        salt_pillar_root: '/srv/pillar',
+        salt_state_top: '/srv/salt/top.sls',
+        state_collection: false,
+        state_top: {},
+        state_top_from_file: false,
+        salt_run_highstate: true,
+        salt_copy_filter: [],
+        is_file_root: false,
+        require_chef: true,
+        dependencies: [],
+        vendor_path: nil,
+        omnibus_cachier: false
+      }
 
       # salt-call version that supports the undocumented --retcode-passthrough command
       RETCODE_VERSION = '0.17.5'.freeze
 
+      DEFAULT_CONFIG.each do |k, v|
+        default_config k, v
+      end
+
       def install_command
         debug(diagnose)
+        salt_version = config[:salt_version]
 
         # if salt_verison is set, bootstrap is being used & bootstrap_options is empty,
         # set the bootstrap_options string to git install the requested version
-        if (config[:salt_version] != 'latest') && (config[:salt_install] == 'bootstrap') && config[:salt_bootstrap_options].empty?
-          debug("Using bootstrap git to install #{config[:salt_version]}")
-          config[:salt_bootstrap_options] = "-P git v#{config[:salt_version]}"
+        if (salt_version != 'latest') && (config[:salt_install] == 'bootstrap') && config[:salt_bootstrap_options].empty?
+          debug("Using bootstrap git to install #{salt_version}")
+          config[:salt_bootstrap_options] = "-P git v#{salt_version}"
         end
 
-        install_template = File.expand_path("./install.erb", File.dirname(__FILE__))
+        install_template = File.expand_path("./../install.erb", __FILE__)
 
         ERB.new(File.read(install_template)).result(binding)
       end
@@ -105,27 +108,7 @@ module Kitchen
         prepare_minion
         prepare_pillars
         prepare_grains
-
-        if config[:state_collection] || config[:is_file_root]
-          prepare_state_collection
-        else
-          prepare_formula config[:kitchen_root], config[:formula]
-
-          unless config[:vendor_path].nil?
-            if Pathname.new(config[:vendor_path]).exist?
-              Dir[File.join(config[:vendor_path], '*')].each do |d|
-                prepare_formula config[:vendor_path], File.basename(d)
-              end
-            else
-              # :vendor_path was set, but not valid
-              raise UserError, "kitchen-salt: Invalid vendor_path set: #{config[:vendor_path]}"
-            end
-          end
-        end
-
-        config[:dependencies].each do |formula|
-          prepare_formula formula[:path], formula[:name]
-        end
+        prepare_states
         prepare_state_top
       end
 
@@ -134,37 +117,41 @@ module Kitchen
         "#{sudo('rm')} -rf #{config[:root_path]} ; mkdir -p #{config[:root_path]}"
       end
 
+      def salt_command
+        salt_version = config[:salt_version]
+        cmd = sudo("salt-call --config-dir=#{File.join(config[:root_path], config[:salt_config])} --local state.highstate")
+        cmd << " --log-level=#{config[:log_level]}" if config[:log_level]
+        if salt_version > RETCODE_VERSION || salt_version == 'latest'
+          # hope for the best and hope it works eventually
+          cmd += ' --retcode-passthrough'
+        end
+        cmd
+      end
+
       def run_command
         debug("running driver #{name}")
         debug(diagnose)
-        if config[:salt_run_highstate]
-          cmd = sudo("salt-call --config-dir=#{File.join(config[:root_path], config[:salt_config])} --local state.highstate")
-        end
-
-        cmd << " --log-level=#{config[:log_level]}" if config[:log_level]
 
         # config[:salt_version] can be 'latest' or 'x.y.z', 'YYYY.M.x' etc
         # error return codes are a mess in salt:
         #  https://github.com/saltstack/salt/pull/11337
         # Unless we know we have a version that supports --retcode-passthrough
         # attempt to scan the output for signs of failure
-        if config[:salt_version] > RETCODE_VERSION || config[:salt_version] == 'latest'
-          # hope for the best and hope it works eventually
-          cmd += ' --retcode-passthrough'
-        else
+        if config[:salt_version] <= RETCODE_VERSION
           # scan the output for signs of failure, there is a risk of false negatives
           fail_grep = 'grep -e Result.*False -e Data.failed.to.compile -e No.matching.sls.found.for'
           # capture any non-zero exit codes from the salt-call | tee pipe
-          cmd = 'set -o pipefail ; ' << cmd
+          cmd = 'set -o pipefail ; ' << salt_command
           # Capture the salt-call output & exit code
           cmd << ' 2>&1 | tee /tmp/salt-call-output ; SC=$? ; echo salt-call exit code: $SC ;'
           # check the salt-call output for fail messages
           cmd << " (sed '/#{fail_grep}/d' /tmp/salt-call-output | #{fail_grep} ; EC=$? ; echo salt-call output grep exit code ${EC} ;"
           # use the non-zer exit code from salt-call, then invert the results of the grep for failures
           cmd << ' [ ${SC} -ne 0 ] && exit ${SC} ; [ ${EC} -eq 0 ] && exit 1 ; [ ${EC} -eq 1 ] && exit 0)'
+          cmd
+        else
+          salt_command         
         end
-
-        cmd
       end
 
       protected
@@ -183,19 +170,9 @@ module Kitchen
       def prepare_minion
         info('Preparing salt-minion')
 
-        minion_config_content = <<-MINION_CONFIG.gsub(/^ {10}/, '')
-          state_top: top.sls
+        minion_template = File.expand_path("./../minion.erb", __FILE__)
 
-          file_client: local
-
-          file_roots:
-           #{config[:salt_env]}:
-             - #{File.join(config[:root_path], config[:salt_file_root])}
-
-          pillar_roots:
-           #{config[:salt_env]}:
-             - #{File.join(config[:root_path], config[:salt_pillar_root])}
-        MINION_CONFIG
+        minion_config_content = ERB.new(File.read(minion_template)).result(binding)
 
         # create the temporary path for the salt-minion config file
         debug("sandbox is #{sandbox_path}")
@@ -204,129 +181,18 @@ module Kitchen
         write_raw_file(sandbox_minion_config_path, minion_config_content)
       end
 
-      def prepare_state_top
-        info('Preparing state_top')
-
-        sandbox_state_top_path = File.join(sandbox_path, config[:salt_state_top])
-
-        if config[:state_top_from_file] == false
-          # use the top.sls embedded in .kitchen.yml
-
-          # we get a hash with all the keys converted to symbols, salt doesn't like this
-          # to convert all the keys back to strings again
-          state_top_content = unsymbolize(config[:state_top]).to_yaml
-          # .to_yaml will produce ! '*' for a key, Salt doesn't like this either
-          state_top_content.gsub!(/(!\s'\*')/, "'*'")
-        else
-          # load a top.sls from disk
-          state_top_content = File.read('top.sls')
-        end
-
-        write_raw_file(sandbox_state_top_path, state_top_content)
-      end
-
-      def prepare_pillars
-        info("Preparing pillars into #{config[:salt_pillar_root]}")
-        debug("Pillars Hash: #{config[:pillars]}")
-
-        return if config[:pillars].nil? && config[:'pillars-from-files'].nil?
-
-        # we get a hash with all the keys converted to symbols, salt doesn't like this
-        # to convert all the keys back to strings again
-        pillars = unsymbolize(config[:pillars])
-        debug("unsymbolized pillars hash: #{pillars}")
-
-        # write out each pillar (we get key/contents pairs)
-        pillars.each do |key, contents|
-          # convert the hash to yaml
-          pillar = contents.to_yaml
-
-          # .to_yaml will produce ! '*' for a key, Salt doesn't like this either
-          pillar.gsub!(/(!\s'\*')/, "'*'")
-
-          # generate the filename
-          sandbox_pillar_path = File.join(sandbox_path, config[:salt_pillar_root], key)
-
-          debug("Rendered pillar yaml for #{key}:\n #{pillar}")
-          write_raw_file(sandbox_pillar_path, pillar)
-        end
-
-        # copy the pillars from files straight across, as YAML.load/to_yaml and
-        # munge multiline strings
-        unless config[:'pillars-from-files'].nil?
-          external_pillars = unsymbolize(config[:'pillars-from-files'])
-          debug("external_pillars (unsymbolize): #{external_pillars}")
-          external_pillars.each do |key, srcfile|
-            debug("Copying external pillar: #{key}, #{srcfile}")
-            # generate the filename
-            sandbox_pillar_path = File.join(sandbox_path, config[:salt_pillar_root], key)
-            # create the directory where the pillar file will go
-            FileUtils.mkdir_p(File.dirname(sandbox_pillar_path))
-            # copy the file across
-            FileUtils.copy srcfile, sandbox_pillar_path
-          end
-        end
-      end
-
       def prepare_grains
         debug("Grains Hash: #{config[:grains]}")
 
         return if config[:grains].nil?
 
         info("Preparing grains into #{config[:salt_config]}/grains")
-        # we get a hash with all the keys converted to symbols, salt doesn't like this
-        # to convert all the keys back to strings again we use unsymbolize
-        # then we convert the hash to yaml
-        grains = unsymbolize(config[:grains]).to_yaml
 
         # generate the filename
         sandbox_grains_path = File.join(sandbox_path, config[:salt_config], 'grains')
         debug("sandbox_grains_path: #{sandbox_grains_path}")
 
         write_hash_file(sandbox_grains_path, config[:grains])
-      end
-
-      def prepare_formula(path, formula)
-        info("Preparing formula: #{formula} from #{path}")
-        debug("Using config #{config}")
-
-        formula_dir = File.join(sandbox_path, config[:salt_file_root], formula)
-        FileUtils.mkdir_p(formula_dir)
-        cp_r_with_filter(File.join(path, formula), formula_dir, config[:salt_copy_filter])
-
-        # copy across the _modules etc directories for python implementation
-        %w(_modules _states _grains _renderers _returners).each do |extrapath|
-          src = File.join(path, extrapath)
-
-          if File.directory?(src)
-            debug("prepare_formula: #{src} exists, copying..")
-            extrapath_dir = File.join(sandbox_path, config[:salt_file_root], extrapath)
-            FileUtils.mkdir_p(extrapath_dir)
-            cp_r_with_filter(src, extrapath_dir, config[:salt_copy_filter])
-          else
-            debug("prepare_formula: #{src} doesn't exist, skipping.")
-          end
-        end
-      end
-
-      def prepare_state_collection
-        info('Preparing state collection')
-        debug("Using config #{config}")
-
-        if config[:collection_name].nil? && config[:formula].nil?
-          info('neither collection_name or formula have been set, assuming this is a pre-built collection')
-          config[:collection_name] = ''
-        elsif config[:collection_name].nil?
-          debug("collection_name not set, using #{config[:formula]}")
-          config[:collection_name] = config[:formula]
-        end
-
-        debug("sandbox_path = #{sandbox_path}")
-        debug("salt_file_root = #{config[:salt_file_root]}")
-        debug("collection_name = #{config[:collection_name]}")
-        collection_dir = File.join(sandbox_path, config[:salt_file_root], config[:collection_name])
-        FileUtils.mkdir_p(collection_dir)
-        cp_r_with_filter(config[:kitchen_root], collection_dir, config[:salt_copy_filter])
       end
     end
   end
