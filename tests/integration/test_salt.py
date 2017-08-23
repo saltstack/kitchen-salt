41a8daa17a4bbf878cfd1e64af14ff435f104295
Daniel Wallace <danielwallace@gtmanfred.com>
2017-08-22 19:21:51 -0600

    skip apt test on centos

--- tests/integration/test_salt.py
+++ tests/integration/test_salt.py
@@ -1,21 +1,29 @@
+import os
+import pytest
+
+
 def test_ping(salt):
     assert salt('test.ping') is True
+
 
 def test_git_depends(salt):
     formulas = {'linux', 'git', 'postfix'}
     dirs = salt('cp.list_master_dirs')
     assert all([formula in dirs for formula in formulas])
 
+@pytest.mark.skipif('centos' in os.environ.get('KITCHEN_INSTANCE'), reason='Skip on centos images')
 def test_apt_depends(salt):
     formulas = {'nginx',}
     dirs = salt('cp.list_master_dirs')
     assert all([formula in dirs for formula in formulas])
+
 
 def test_spm_depends(salt):
     formulas = {'hubblestack_nova'}
     dirs = salt('cp.list_master_dirs')
     print(dirs)
     assert all([formula in dirs for formula in formulas])
+
 
 def test_path_depends(salt):
     formulas = {'foo',}
