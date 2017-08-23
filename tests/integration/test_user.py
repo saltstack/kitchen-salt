7ebc81e56fccb6a53360ee02133d67e2e5b6d491
Daniel Wallace <danielwallace@gtmanfred.com>
2017-08-23 07:40:22 -0600

    attempt to add freebsd
    And also test it

--- tests/integration/test_user.py
+++ tests/integration/test_user.py
@@ -1,3 +1,8 @@
+import os
+import pytest
+
+
+@pytest.mark.skipif('freebsd' in os.environ.get('KITCHEN_INSTANCE'), reason='Skip on freebsd images')
 def test_jdoe(host):
     jdoe = host.user('jdoe')
     assert jdoe.name == 'jdoe'
