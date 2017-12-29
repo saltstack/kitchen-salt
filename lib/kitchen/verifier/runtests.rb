750bed455ef93ff2ad254162124489294da7e77b
Daniel Wallace <danielwallace@gtmanfred.com>
2017-12-29 13:10:12 -0700

    add runtests verifier for salt

---
+++ lib/kitchen/verifier/runtests.rb
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+
+require "kitchen/verifier/base"
+
+module Kitchen
+  module Verifier
+    class Runtests < Kitchen::Verifier::Base
+      kitchen_verifier_api_version 1
+
+      plugin_version Kitchen::VERSION
+
+      default_config :testingdir, '/tmp/kitchen/testing'
+      default_config :python_bin, 'python2'
+      default_config :verbose, false
+      default_config :run_destructive, false
+      default_config :xml, false
+      default_config :coverage_xml, false
+      default_config :types, []
+      default_config :transport, false
+
+      def call(state)
+        info("[#{name}] Verify on instance #{instance.name} with state=#{state}")
+        command = [
+          config[:python_bin],
+          File.join(config[:testingdir], '/tests/runtests.py'),
+          '--sysinfo',
+          '--output-columns=80',
+          (config[:transport] ? "--transport=#{config[:transport]}" : ''),
+          (config[:verbose] ? '-v' : ''),
+          (config[:run_destructive] ? "--run-destructive" : ''),
+          (config[:coverage_xml] ? "--coverage-xml=#{config[:coverage_xml]}" : ''),
+          (config[:xml] ? "--xml=#{config[:xml]}" : ''),
+          config[:types].collect{|type| "--#{type}"}.join(' '),
+          config[:tests].collect{|test| "-n #{test}"}.join(' '),
+        ].join(' ')
+        instance.transport.connection(state) do |conn|
+          conn.execute(sudo(command))
+        end
+        debug("[#{name}] Verify completed.")
+      end
+    end
+  end
+end
